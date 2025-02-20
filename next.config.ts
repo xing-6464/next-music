0091506e06b0ef9d95361b95e313814ddea9260a
xing-6464 <xingguang64@yeah.net>
2025-02-21 00:21:15 +0800

    init

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   /* config options here */
-};
+  sassOptions: {
+    additionalData: `@import "./src/assets/scss/variable.scss"; @import "./src/assets/scss/mixin.scss";`,
+  },
+}
 
-export default nextConfig;
+export default nextConfig
